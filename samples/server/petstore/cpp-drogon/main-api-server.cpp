eeccbb8b5a5e9634db4604b7ae8c12d0318d1ac1
Karim Dhrif <karimdhrif4444@gmail.com>
2025-07-24 17:16:11 +0200

    Generating the main-api-server for drogon done

--- samples/server/petstore/cpp-drogon/main-api-server.cpp
+++ samples/server/petstore/cpp-drogon/main-api-server.cpp
@@ -10,88 +10,54 @@
 * Do not edit the class manually.
 */
 
+#include <drogon/drogon.h>
+#include <memory>
+    #include <thread>
 
-#include "pistache/endpoint.h"
-#include "pistache/http.h"
-#include "pistache/router.h"
-#ifdef __linux__
-#include <vector>
-#include <signal.h>
-#include <unistd.h>
-#endif
-
-#include "ApiBase.h"
-
-#include "PetApiImpl.h"
-#include "StoreApiImpl.h"
-#include "UserApiImpl.h"
-
-#define PISTACHE_SERVER_THREADS     2
-#define PISTACHE_SERVER_MAX_REQUEST_SIZE 32768
-#define PISTACHE_SERVER_MAX_RESPONSE_SIZE 32768
-
-static Pistache::Http::Endpoint *httpEndpoint;
-#ifdef __linux__
-static void sigHandler [[noreturn]] (int sig){
-    switch(sig){
-        case SIGINT:
-        case SIGQUIT:
-        case SIGTERM:
-        case SIGHUP:
-        default:
-            httpEndpoint->shutdown();
-            break;
-    }
-    exit(0);
-}
-
-static void setUpUnixSignals(std::vector<int> quitSignals) {
-    sigset_t blocking_mask;
-    sigemptyset(&blocking_mask);
-    for (auto sig : quitSignals)
-        sigaddset(&blocking_mask, sig);
-
-    struct sigaction sa;
-    sa.sa_handler = sigHandler;
-    sa.sa_mask    = blocking_mask;
-    sa.sa_flags   = 0;
-
-    for (auto sig : quitSignals)
-        sigaction(sig, &sa, nullptr);
-}
-#endif
-
-using namespace org::openapitools::server::api;
-
-int main() {
-#ifdef __linux__
-    std::vector<int> sigs{SIGQUIT, SIGINT, SIGTERM, SIGHUP};
-    setUpUnixSignals(sigs);
-#endif
-    Pistache::Address addr(Pistache::Ipv4::any(), Pistache::Port(8080));
-
-    httpEndpoint = new Pistache::Http::Endpoint((addr));
-    auto router = std::make_shared<Pistache::Rest::Router>();
-
-    auto opts = Pistache::Http::Endpoint::options()
-        .threads(PISTACHE_SERVER_THREADS);
-    opts.flags(Pistache::Tcp::Options::ReuseAddr);
-    opts.maxRequestSize(PISTACHE_SERVER_MAX_REQUEST_SIZE);
-    opts.maxResponseSize(PISTACHE_SERVER_MAX_RESPONSE_SIZE);
-    httpEndpoint->init(opts);
-
-    auto apiImpls = std::vector<std::shared_ptr<ApiBase>>();
     
-    apiImpls.push_back(std::make_shared<PetApiImpl>(router));
-    apiImpls.push_back(std::make_shared<StoreApiImpl>(router));
-    apiImpls.push_back(std::make_shared<UserApiImpl>(router));
+        #include "PetApi.h"          // Generated controller
+    
+        #include "StoreApi.h"          // Generated controller
+    
+        #include "UserApi.h"          // Generated controller
+    
 
-    for (auto api : apiImpls) {
-        api->init();
+    using namespace org::openapitools::server::api;
+
+    int
+    main(int /*argc*/, char* /*argv*/[])
+    {
+    /* -----------------------------------------------------------------------
+    * 1. Core server settings
+    * -------------------------------------------------------------------- */
+    const unsigned short port =
+    static_cast<unsigned short>(8080);
+    const unsigned int threads =
+    std::max(1u, std::thread::hardware_concurrency());   // sensible default
+
+    drogon::app()
+    .setThreadNum(threads)               // worker‑threads
+    .addListener("0.0.0.0", port)        // bind address / port
+    /* .enableRunAsDaemon()              // ← uncomment if you need daemon mode   */
+    /* .setSSLFiles("server.pem","key")  // ← TLS example                        */
+    ;
+
+    /* -----------------------------------------------------------------------
+    * 2. Register controllers
+    *    (second template parameter = false ⇒ manual registration required)
+    * -------------------------------------------------------------------- */
+    
+        drogon::app().registerController(std::make_shared<PetApi>());
+    
+        drogon::app().registerController(std::make_shared<StoreApi>());
+    
+        drogon::app().registerController(std::make_shared<UserApi>());
+    
+
+    /* -----------------------------------------------------------------------
+    * 3. Start event loop (blocking)
+    * -------------------------------------------------------------------- */
+    drogon::app().run();
+
+    return 0;
     }
-
-    httpEndpoint->setHandler(router->handler());
-    httpEndpoint->serve();
-
-    httpEndpoint->shutdown();
-}
